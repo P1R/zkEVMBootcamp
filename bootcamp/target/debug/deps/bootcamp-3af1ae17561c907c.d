b151b2f4fbfbc55375e1a0f87cd07130fde0ab0e
David E. Perez Negron R. <david@nethunters.xyz>
2023-11-28 22:58:26 -0600

    Adding Homework 6

---
+++ bootcamp/target/debug/deps/bootcamp-3af1ae17561c907c.d
@@ -0,0 +1,5 @@
+/home/p1r0/Dev/labs/encode/zkEVMBootcamp/bootcamp/target/debug/deps/bootcamp-3af1ae17561c907c.rmeta: src/main.rs
+
+/home/p1r0/Dev/labs/encode/zkEVMBootcamp/bootcamp/target/debug/deps/bootcamp-3af1ae17561c907c.d: src/main.rs
+
+src/main.rs:
---
+++ bootcamp/target/debug/deps/bootcamp-a9005a24044c385d.d
@@ -0,0 +1,5 @@
+/home/p1r0/Dev/labs/encode/zkEVMBootcamp/bootcamp/target/debug/deps/bootcamp-a9005a24044c385d: src/main.rs
+
+/home/p1r0/Dev/labs/encode/zkEVMBootcamp/bootcamp/target/debug/deps/bootcamp-a9005a24044c385d.d: src/main.rs
+
+src/main.rs:
