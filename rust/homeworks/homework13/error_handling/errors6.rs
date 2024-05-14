00fd804356738064bf7cc0bc5ce6428b4b2d5fa2
David E. Perez Negron R. <david@nethunters.xyz>
2024-05-13 22:56:06 -0600

    Adding Homework 13

--- rust/homeworks/homework13/error_handling/errors6.rs
+++ rust/homeworks/homework13/error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -20,15 +19,26 @@ enum ParsePosNonzeroError {
 }
 
 impl ParsePosNonzeroError {
+    fn from_creation(err: CreationError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::Creation(err)
+    }
     // TODO: add another error conversion function here.
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str)
-    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
-{
+    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    //let x: i64 = s.parse().unwrap();
+
+    let x: i64 = match s.parse() {
+        Ok(x) => x, 
+        Err(error) => return Err(ParsePosNonzeroError::from_parseint(error))
+    };
+
     PositiveNonzeroInteger::new(x)
         .map_err(ParsePosNonzeroError::from_creation)
 }
